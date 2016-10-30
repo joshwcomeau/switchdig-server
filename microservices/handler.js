04bd6379affdbc982ebe43dbda62b74d31dc322a
Joshua Comeau <joshwcomeau@gmail.com>
2016-10-30 18:55:26 -0400

    Finish hello-world serverless example

--- microservices/handler.js
+++ microservices/handler.js
@@ -1,10 +1,31 @@
 'use strict';
 
-module.exports.getUser = (event, context, callback) => {
+module.exports.listUsers = (event, context, callback) => {
   const response = {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
+    },
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Go Serverless v1.0! GET listUsers!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+module.exports.createUser = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! POST createUser!',
       input: event,
     }),
   };
