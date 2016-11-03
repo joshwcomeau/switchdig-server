de4728ba83077d74885e1775e519dccb7e6bcbb8
Joshua Comeau <joshwcomeau@gmail.com>
2016-11-03 08:18:47 -0400

    Filter searched authors to find the best match

--- handlers/search-author.js
+++ handlers/search-author.js
@@ -11,6 +11,7 @@ module.exports = (event, context, callback) => {
       //
       const books = result
         .map(book => {
+          const id = _.get(book, 'ASIN[0]');
           const author = _.get(book, 'ItemAttributes[0].Author[0]');
           const title = _.get(book, 'ItemAttributes[0].Title[0]');
           const image = _.get(book, 'LargeImage[0].URL[0]');
@@ -21,13 +22,21 @@ module.exports = (event, context, callback) => {
 
           // TODO: Filter by mediaType
 
-          return { author, title, image };
+          return { id, author, title, image };
         })
         .filter(book => !!book);
 
+      // It's possible that, for example, 2 of the 10 results are from another
+      // author with a similar name. We want to only return the books from the
+      // most popular author.
+      const authorsByCount = _.countBy(books, 'author');
+      const maxRepeated = _.max(_.values(authorsByCount));
+      const author = _.findKey(authorsByCount, count => count === maxRepeated);
+      const filteredBooks = books.filter(book => book.author === author);
+
       const response = respond(200, {
-        books,
-        author: event.queryStringParameters.author,
+        author,
+        books: filteredBooks,
       })
 
       callback(null, response);
