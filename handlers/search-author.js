7757b18b823f52daa353451f94b8492e69639864
Joshua Comeau <joshwcomeau@gmail.com>
2016-11-05 09:46:02 -0400

    Set searchAuthor to take a perfect match when available

--- handlers/search-author.js
+++ handlers/search-author.js
@@ -1,10 +1,14 @@
+'use strict';
+
 const _ = require('lodash');
 
 const search = require('../helpers/product-advertising-api').search;
 const respond = require('../helpers/respond');
 
 module.exports = (event, context, callback) => {
-  search({ author: event.queryStringParameters.author })
+  const searchedAuthor = event.queryStringParameters.author;
+
+  search({ author: searchedAuthor })
     .then(result => {
       // AWS returns a bunch of garbage we don't need.
       // Pluck out the valuable info, and return it to the client.
@@ -26,12 +30,25 @@ module.exports = (event, context, callback) => {
         })
         .filter(book => !!book);
 
-      // It's possible that, for example, 2 of the 10 results are from another
-      // author with a similar name. We want to only return the books from the
-      // most popular author.
-      const authorsByCount = _.countBy(books, 'author');
-      const maxRepeated = _.max(_.values(authorsByCount));
-      const author = _.findKey(authorsByCount, count => count === maxRepeated);
+      // If the supplied author name is a perfect match for any of the authors,
+      // take that one. Sometimes the most popular author is wrong!
+      const perfectMatchBook = books.find(book => (
+        book.author.toLowerCase() === searchedAuthor.toLowerCase()
+      ));
+
+      let author;
+
+      if (perfectMatchBook) {
+        author = perfectMatchBook.author;
+      } else {
+        // If no perfect match was found, the most likely author is the one
+        // with the most results. Let's trust Amazon's relevancy for this.
+        const authorsByCount = _.countBy(books, 'author');
+        const maxRepeated = _.max(_.values(authorsByCount));
+
+        author = _.findKey(authorsByCount, count => count === maxRepeated);
+      }
+
       const filteredBooks = books.filter(book => book.author === author);
 
       const response = respond(200, {
